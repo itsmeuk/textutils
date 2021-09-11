a275de41c003bb8a1a85740026035eae5f82ea84
Uk <umeshk1176@gmail.com>
2021-09-11 19:14:50 +0530

    added a new component named TextForm; used States and created a button to convert the entered text in UPPERCASE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
+    {/* Navbar */}
     <Navbar title="TextUtils" aboutTab="About Text Utils" />
+
+    {/* TextArea */}
+    <div className="container my-3">
+    <TextForm heading="Enter your text here!" />
+    </div>
     </>
   );
 }
