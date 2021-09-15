ca77be4645788e199db618e44b48c27ea378de14
Uk <umeshk1176@gmail.com>
2021-09-16 02:39:10 +0530

    Alert component added to show alerts.

--- src/App.js
+++ src/App.js
@@ -3,32 +3,47 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 
 function App() {
   const [mode, setMode] = useState('light'); //whether darkmode is enabled or not
-  
-  const toggleMode = () =>{
-    if(mode === 'light'){
-      setMode ('dark');
-      document.body.style.backgroundColor = '#042743'
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 3000);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled!", "success")
     }
-    else{
-      setMode ('light');
-      document.body.style.backgroundColor = 'white'
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled!", "success")
     }
   }
 
   return (
     <>
-    {/* Navbar */}
-    <Navbar title="TextUtils" aboutTab="About Text Utils" mode={mode} toggleMode={toggleMode} />
+      {/* Navbar */}
+      <Navbar title="TextUtils" aboutTab="About Text Utils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert= {alert} />
 
-    {/* TextArea */}
-    <div className="container my-3">
-    <TextForm heading="Enter your text here!" mode={mode} />
-    {/* <About/> */}
-    </div>
+      {/* TextArea */}
+      <div className="container my-3">
+        <TextForm showAlert={showAlert} heading="Enter your text here!" mode={mode} />
+        {/* <About/> */}
+      </div>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,24 +6,28 @@ export default function TextForm(props) {
         // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Text converted to Uppercase!", "success");
     }
-
+    
     const handleLoClick = ()=>{
         // console.log("lowercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Text converted to Lowercase!", "success");
     }
-
-const handleCopy = ()=>{
-    // console.log("Copy button was clicked" + text);
-    var text = document.getElementById("myTextBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-}
-
+    
+    const handleCopy = ()=>{
+        // console.log("Copy button was clicked" + text);
+        var text = document.getElementById("myTextBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to clipboard!, use ctrl+v to paste the copied text.", "success");
+    }
+    
     const handleClearClick = ()=>{
         // console.log("Erase button was clicked" + text);
         let newText = '';
+        props.showAlert("You cleared the textbox!", "warning");
         setText(newText);
     }
 
@@ -35,17 +39,17 @@ const handleCopy = ()=>{
     const [text, setText] = useState('');
     return (
         <>
-        <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
+        <div className="container" style={{color: props.mode === 'dark'?'white':'#042743'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'black'}} id="myTextBox" rows="10"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'#042743'}} id="myTextBox" rows="10"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UPPERCASE</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to lowercase</button>
             <button className="btn btn-secondary mx-5" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-danger mx-3" onClick={handleClearClick}>Erase Text</button>
         </div>
-        <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
+        <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
             <p>You have entered {text.split(" ").length} words and {text.length} characters (including space).</p>
             <p>You need average time of {0.008*text.split(" ").length} minutes to read this enough.</p>
