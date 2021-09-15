f510d18d567370e34963c68d335da7b2ef0eb9f1
Uk <umeshk1176@gmail.com>
2021-09-16 01:47:19 +0530

    added dark theme to whole body, added dark mode switch to navbar and improved dark mode

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState('light'); //whether darkmode is enabled or not
+  
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode ('dark');
+      document.body.style.backgroundColor = '#042743'
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor = 'white'
+    }
+  }
+
   return (
     <>
     {/* Navbar */}
-    <Navbar title="TextUtils" aboutTab="About Text Utils" />
+    <Navbar title="TextUtils" aboutTab="About Text Utils" mode={mode} toggleMode={toggleMode} />
 
     {/* TextArea */}
     <div className="container my-3">
-    <TextForm heading="Enter your text here!" />
+    <TextForm heading="Enter your text here!" mode={mode} />
     {/* <About/> */}
     </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -35,23 +35,23 @@ const handleCopy = ()=>{
     const [text, setText] = useState('');
     return (
         <>
-        <div className="container">
+        <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myTextBox" rows="10"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'black'}} id="myTextBox" rows="10"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UPPERCASE</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to lowercase</button>
             <button className="btn btn-secondary mx-5" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-danger mx-3" onClick={handleClearClick}>Erase Text</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
             <h2>Your text summary</h2>
             <p>You have entered {text.split(" ").length} words and {text.length} characters (including space).</p>
             <p>You need average time of {0.008*text.split(" ").length} minutes to read this enough.</p>
             <br />
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the box above to preview that here!"}</p>
         </div>
         </>
     )
